e57f4f2aa22753ddc65682c3f87c7feb0b256e80
nitingera1996 <nitin.gera.ece14@iitbhu.ac.in>
2016-03-03 17:27:20 +0530

    send_sms added

--- app/views.py
+++ app/views.py
@@ -8,6 +8,7 @@ import json
 import time
 #import serial
 import urllib2
+import cookielib
 '''
 ser1 = serial.Serial(port = '/dev/ttyUSB0',baudrate = 9600, parity = serial.PARITY_NONE,stopbits=serial.STOPBITS_ONE,bytesize=serial.EIGHTBITS,timeout=1)
 ser2 = serial.Serial(port = '/dev/ttyACM0',baudrate = 115200, parity = serial.PARITY_NONE,stopbits=serial.STOPBITS_ONE,bytesize=serial.EIGHTBITS,timeout=1)
@@ -372,3 +373,30 @@ def myinfo(pnr):
    return render_template('myyatrainfo.html',title='MyYatraInfo',values=response_dict,login=login)
 
 
+def send_sms(number,message):
+  username="8272859757"
+  passwd="M2228S"
+  message = "+".join(message.split(' '))
+  url = 'http://site24.way2sms.com/Login1.action?'
+  data = 'username='+username+'&password='+passwd+'&Submit=Sign+in'
+
+  cj = cookielib.CookieJar()
+  opener = urllib2.build_opener(urllib2.HTTPCookieProcessor(cj))
+
+  opener.addheaders = [('User-Agent','Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/37.0.2062.120 Safari/537.36')]
+
+  try:
+      usock = opener.open(url, data)
+  except IOError:
+      return False
+
+  jession_id = str(cj).split('~')[1].split(' ')[0]
+  send_sms_url = 'http://site24.way2sms.com/smstoss.action?'
+  send_sms_data = 'ssaction=ss&Token='+jession_id+'&mobile='+number+'&message='+message+'&msgLen=136'
+  opener.addheaders = [('Referer', 'http://site25.way2sms.com/sendSMS?Token='+jession_id)]
+
+  try:
+      sms_sent_page = opener.open(send_sms_url,send_sms_data)
+  except IOError:
+      return False
+  return True
--- populate_chart.py
+++ populate_chart.py
@@ -1,8 +1,11 @@
 from app import db,models
+from app.views import send_sms
 u1 = models.Chart(name='Nitin',pnr=1409503636,mobile='8272859757',password='3636',seat=3)
+#send_msg(u1.mobile,"Your password is "+u1.password+" - Indian Railways")
 u2 = models.Chart(name='Aman',pnr=1409500404,mobile='8874671844',password='0404',seat=4)
+send_sms(u2.mobile,"Your password is "+u2.password+" - Indian Railways")
 u3 = models.Chart(name='Harsh',pnr=1409501818,mobile='7754812482',password='1818',seat=18)
-u3 = models.Chart(name='Om',pnr=1409503737,mobile='7607986662',password='3737',seat=12)
+u4 = models.Chart(name='Om',pnr=1409503737,mobile='7607986662',password='3737',seat=12)
 db.session.add(u1)
 db.session.add(u2)
 db.session.add(u3)
